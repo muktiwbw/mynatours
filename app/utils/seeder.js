10a9ae51ca1e9576e69eabc67679f7f7cbcefaa0
Mukti Wibowo <muktiw7@gmail.com>
2020-07-12 07:16:06 +0700

    Added database seeder

---
+++ app/utils/seeder.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const User = require('./../models/userModel');
+const Tour = require('./../models/tourModel');
+const { catchAsync } = require('./query');
+const { promisify } = require('util');
+
+const seed = catchAsync(async () => {
+  const usersSeed = JSON.parse(await promisify(fs.readFile)(`${__dirname}/../seeds/users.json`, { encoding: 'utf-8' }));
+  const toursSeed = JSON.parse(await promisify(fs.readFile)(`${__dirname}/../seeds/tours.json`, { encoding: 'utf-8' }));
+
+  const users = await User.insertMany(usersSeed);
+  const tours = await Tour.insertMany(toursSeed);
+
+  Promise.all([ users, tours ]).then(() => {
+    console.log('Database seeding is completed');
+    process.exit();
+  }).catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
+})
+
+const scoop = catchAsync(async () => {
+  const users = await User.deleteMany();
+  const tours = await Tour.deleteMany();
+
+  Promise.all([ users, tours ]).then(() => {
+    console.log('Database scooping is completed');
+    process.exit();
+  }).catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
+});
+
+if (process.argv[2] === 'seed') {
+  seed();
+} else if (process.argv[2] === 'scoop') {
+  scoop();
+} else {
+  console.log(`Invalid flag: ${process.argv[2]}`);
+  process.exit();
+}

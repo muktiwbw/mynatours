3e7a06605d9df872c908262f6b7a7920070f9bd6
Mukti Wibowo <muktiw7@gmail.com>
2020-07-12 09:15:01 +0700

    Added unique index for tour and user

---
+++ app/models/reviewModel.js
@@ -0,0 +1,27 @@
+const db = require('./../utils/db');
+
+const schema = new db.Schema({
+  review: {
+    type: String,
+    required: [ true, 'Missing review field' ],
+    trim: true
+  },
+  rating: {
+    type: Number,
+    required: [ true, 'Missing rating field' ],
+    min: 0,
+    max: 5
+  },
+  user: {
+    type: db.Schema.ObjectId,
+    ref: 'User'
+  },
+  tour: {
+    type: db.Schema.ObjectId,
+    ref: 'Tour'
+  }
+});
+
+schema.index({ tour: 1, user: 1 }, { unique: true });
+
+module.exports = db.model('Review', schema, 'reviews');

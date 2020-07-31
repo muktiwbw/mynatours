71fedd357797e190dd8dd5c14de95aeb96717919
Mukti Wibowo <muktiw7@gmail.com>
2020-07-31 17:23:53 +0700

    Removing host from env

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./app/app');
 
-app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
-  console.log(`Listening to ${process.env.APP_HOST}:${process.env.APP_PORT}...`)
+app.listen(process.env.PORT, () => {
+  console.log(`Listening to port ${process.env.APP_PORT}...`)
 });
